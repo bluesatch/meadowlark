980b13f0a2faf90e7d69e08ed50b229d7b2dd716
H Brown <hebrown12@gmail.com>
2022-02-05 16:00:34 -0600

    completed chapter 3

--- index.js
+++ index.js
@@ -1,36 +1,81 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
+
+/**
+ * Array of Fortunes
+ */
+
+const fortunes = [
+    'Conquer your fears or they will conquer you',
+    'Rivers need springs',
+    'Do not fear what you don\'t know',
+    'You will have a pleasant surprise',
+    'Whenever possible, keep it simple'
+]
+
+app.use(express.static(__dirname + '/public'))
+
+// Configure hadlebars view engine
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main'
+}))
+
+app.set('view engine', 'handlebars')
 
 /**
  * Routes
  */
 // get and post two most common
 // two parameters: path and a function
-app.get('/', (req, res)=> {
-    // console.log('home')
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-})
+// app.get('/', (req, res)=> {
+//     // console.log('home')
+//     res.type('text/plain')
+//     res.send('Meadowlark Travel')
+// })
 
+// app.get('/about', (req, res)=> {
+//     res.type('text/plain')
+//     res.send('About Meadowlark Travel')
+// })
+
+// // custom 404 page 
+// app.use((req, res)=> {
+//     res.type('text/plain')
+//     res.status(404)
+//     res.send('404 - Not Found')
+// })
+
+// // custom 500 page 
+// app.use((err, req, res, next)=> {
+//     console.error(err.message)
+//     res.type('text/plain')
+//     res.status(500)
+//     res.send('500 - Server Error')
+// })
+
+/*
+ * Replace old routes with new ones using views
+ */
+app.get('/', (req, res) => res.render('home'))
+
+// app.get('/about', (req, res) => res.render('about'))
+// modify route to about
 app.get('/about', (req, res)=> {
-    res.type('text/plain')
-    res.send('About Meadowlark Travel')
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', {fortune: randomFortune})
 })
 
-// custom 404 page 
-app.use((req, res)=> {
-    res.type('text/plain')
+app.use((req, res) => {
     res.status(404)
-    res.send('404 - Not Found')
+    res.render('404')
 })
 
-// custom 500 page 
-app.use((err, req, res, next)=> {
+app.use((err, req, res, next) => {
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - Server Error')
+    res.render('500')
 })
 
 app.listen(port, ()=> console.log(
