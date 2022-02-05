fd4d8c3e77ed41c6f930ee0b2f7f78bf5559e38f
H Brown <hebrown12@gmail.com>
2022-02-04 22:32:26 -0600

    built first express server

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+/**
+ * Routes
+ */
+// get and post two most common
+// two parameters: path and a function
+app.get('/', (req, res)=> {
+    // console.log('home')
+    res.type('text/plain')
+    res.send('Meadowlark Travel')
+})
+
+app.get('/about', (req, res)=> {
+    res.type('text/plain')
+    res.send('About Meadowlark Travel')
+})
+
+// custom 404 page 
+app.use((req, res)=> {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
+})
+
+// custom 500 page 
+app.use((err, req, res, next)=> {
+    console.error(err.message)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
+})
+
+app.listen(port, ()=> console.log(
+    `Express started on http://localhost:${port}, ` + `press Ctrl-C to terminate...`
+))
