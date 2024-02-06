1212ee5ebc3878e7c273c2651ac4ef308b853e6e
H Brown <hebrown12@gmail.com>
2024-02-06 15:19:37 -0600

    Moved 'fortune cookie' into module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 // import handlebars
 const handlebars = require('express-handlebars')
+
+// importing getFortune 
+const fortune = require('./lib/fortune')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -17,13 +20,7 @@ const port = process.env.PORT || 3000
  * IN EXPRESS, THE ORDER IN WHICH ROUTES AND MIDDLEWARE ARE ADDED IS SIGNIFICANT
  */
 
-const fortunes = [
-    "Conquer your fears or they will conquer you",
-    "Rivers need springs",
-    "Do not fear what you don't know",
-    "You will have a pleasant surprise",
-    "Whenever possible, keep it simple"
-]
+
 
 // add the static middleware 
 app.use(express.static(__dirname + '/public'))
@@ -44,7 +41,7 @@ app.get('/', (req, res)=> res.render('home'))
 
 app.get('/about', (req, res)=> {
     const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-    res.render('about', { fortune: randomFortune })
+    res.render('about', { fortune: fortune.getFortune() })
 })
 // app.get('/about', (req, res)=> {
 //     res.type('text/plain')
